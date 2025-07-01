def971269a0e94b2179110da9a519504b58b3e9d
kartikeyGehlot2328 <kartikey.22bcon777@jecrcu.edu.in>
2025-07-01 22:50:34 +0530

    updated styling 02

--- src/Projects.jsx
+++ src/Projects.jsx
@@ -6,7 +6,7 @@ export default function Projects({projHdng, projDscrptn,videoSrc ,projectId, lin
 
             <video autoPlay loop muted className="h-full object-contain rounded-[10px] border-[1px] border-amber-50" src={videoSrc}/>
             
-            <p className="w-full text-center text-[15px] text-justify">
+            <p className="w-full text-[15px] text-justify">
                 {projDscrptn} 
             </p>
        </a>
