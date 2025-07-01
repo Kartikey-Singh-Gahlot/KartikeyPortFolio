cad923429237917107827ee3989b2ff3701446b6
kartikeyGehlot2328 <kartikey.22bcon777@jecrcu.edu.in>
2025-07-01 22:39:59 +0530

    updated project links

--- src/Projects.jsx
+++ src/Projects.jsx
@@ -1,7 +1,7 @@
 
-export default function Projects({projHdng, projDscrptn,videoSrc ,projectId}){
+export default function Projects({projHdng, projDscrptn,videoSrc ,projectId, link}){
     return(
-       <div  id={projectId} className="border-amber-50 hover:bg-black hover:border-[1px] relative flex flex-col gap-10  shadow-black box-border  rounded-[10px] justify-center px-3 pt-3 pb-10 text-amber-50 ">
+       <a href={link} id={projectId} className="border-amber-50 hover:bg-black hover:border-[1px] relative flex flex-col gap-10  shadow-black box-border  rounded-[10px] justify-center px-3 pt-3 pb-10 text-amber-50 ">
             <h1 className="text-center w-full text-2xl">{projHdng}</h1>
 
             <video autoPlay loop muted className="h-full object-contain rounded-[10px] border-[1px] border-amber-50" src={videoSrc}/>
@@ -9,6 +9,6 @@ export default function Projects({projHdng, projDscrptn,videoSrc ,projectId}){
             <p className="w-full text-center text-[15px]">
                 {projDscrptn} 
             </p>
-       </div>
+       </a>
     );
 }
