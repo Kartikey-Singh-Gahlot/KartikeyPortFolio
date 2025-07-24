5099452d2702a48366204991e9af83076104c39e
kartikeyGehlot2328 <kartikey.22bcon777@jecrcu.edu.in>
2025-07-24 12:58:51 +0530

    underline to heading added

--- src/Experience.jsx
+++ src/Experience.jsx
@@ -1,8 +1,9 @@
-export default function Experience({experienceTitle,experienceDescription,experienceBoxStyle}){
+export default function Experience({experienceTitle,experienceTimeline,experienceDescription,experienceBoxStyle}){
   
   return (
     <div className={experienceBoxStyle}>
-        <h1 className="min-[780px]:text-[18px] text-[12px] font-bold ">{experienceTitle}</h1>
+        <h1 className="min-[780px]:text-[18px] text-[12px] font-bold">{experienceTitle}</h1>
+        <h1 className="min-[780px]:text-[18px] text-[12px] font-bold">{experienceTimeline}</h1>
          <ul className="my-3 min-[780px]:list-none list-disc">
              {experienceDescription.map((i)=>{ return <li>{i}</li>})}
          </ul>
--- src/Headings.jsx
+++ src/Headings.jsx
@@ -1,10 +1,12 @@
 export default function Headings({pageHeading, pageHeadingStyle}){
 
- let crntStyle = "min-[430px]:text-4xl text-3xl  text-amber-50 text-center my-5";
+ let crntStyle = "min-[430px]:text-4xl text-3xl  text-amber-50 text-center underline my-5";
 
  if(pageHeadingStyle){
     crntStyle = crntStyle.concat(pageHeadingStyle);
  }
 
- return <h1 className={crntStyle}>{pageHeading}</h1>
+ return (
+      <h1 className={crntStyle}>{pageHeading}</h1>
+ )
 }
