ebcd7d0ebb3aac35f54916e07a1fbc6b8ff9def8
Kartikey Singh Gahlot <decten.kartz@gmail.com>
2025-08-05 23:01:49 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -5,7 +5,7 @@ const app = express();
 const cors = require('cors');
 
 app.use(cors({
-  origin: ["https://kartikey-portfolio-frontend.vercel.app/"]
+  origin: true
 }));
 
 connectDb().then(()=> console.log("database connected")).catch((err)=>{console.log(err)});
