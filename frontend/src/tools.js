afd446f752e1b41c465a213fd8606fe98978b4c0
Kartikey Singh Gahlot <decten.kartz@gmail.com>
2025-08-20 22:59:41 +0530

    Update tools.js

--- frontend/src/tools.js
+++ frontend/src/tools.js
@@ -4,7 +4,7 @@ export const mailLink = "https://mail.google.com/mail/?view=cm&fs=1&to=2002gehlo
 export const myPicLink = "/kartikeyPic.png";
 
 
-export const headerData = ["z-20 bg-gradient-to-r from-[#121d31] to-black fixed top-0 h-fit w-full flex justify-between items-center box-border py-5 px-10","z-20 bg-gradient-to-r from-black to-black fixed top-0 h-screen justify-center  w-full flex flex-col box-border py-5 px-10 border-b-[1px] border-amber-50 "];
+export const headerData = ["z-20 bg-gradient-to-r from-[#121d31] to-black fixed top-0 h-fit w-full flex justify-between items-center box-border py-5 px-10","z-20 bg-gradient-to-r from-black to-black fixed top-0 h-screen w-full flex flex-col box-border py-5 px-10 border-b-[1px] border-amber-50 "];
 export const hrOldData = [];
 export const hrNewData = [];
 export const navData = ["min-[790px]:flex hidden justify-between","min-[790px]:flex flex flex-col my-5 justify-evenly"];
