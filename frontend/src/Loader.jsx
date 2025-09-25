d4e88af6b8b0a1d8d874d468d2a97407d9f4785b
Kartikey Singh Gahlot <decten.kartz@gmail.com>
2025-09-25 10:04:47 +0530

    changed loader style

--- frontend/src/Loader.jsx
+++ frontend/src/Loader.jsx
@@ -1,13 +1,11 @@
 export default function Loader(){
     return(
        <div className="w-full flex  text-2xl justify-center items-center py-10 relative gap-5">
-          <h1 className="loadingTextAnimation text-white">Loading.... </h1>
-           <div className="h-20 w-20   flex justify-center items-center rounded-full relative border-5 border-b-white animate-spin box-border">
-                <div className="h-15 w-15 rounded-full border-5  border-b-[#ffffff8d] border-dotted border-l-white  box-border">
-                    
-                </div>
-           </div>
-          
+         
+           <div className="h-12 w-12   flex justify-center items-center rounded-full relative border-4  border-b-white animate-spin box-border">
+    
+           </div>    
+            <h1 className="loadingTextAnimation text-white loadingText relative">Loading</h1>  
         </div>
     );
 }
--- frontend/src/tools.js
+++ frontend/src/tools.js
@@ -2,7 +2,7 @@
 export const cvLink = "/KartikeySinghGahlotCv.pdf";
 export const mailLink = "https://mail.google.com/mail/?view=cm&fs=1&to=2002gehlotkartikeysingh@gmail.com&su=Hiring&body=Hi%20Kartikey";
 export const myPicLink = "/kartikeyPic.png";
-export const backendApiBaseUrl = "https://kartikey-portfolio-backend.vercel.app";
+export const backendApiBaseUrl = "https://kartikey-portfolio-backend.vercel.ap";
 
 
 export const headerData = ["z-20 bg-gradient-to-r from-[#121d31] to-black sticky top-0 h-fit w-full flex justify-between items-center box-border py-5 px-10 ","z-20 bg-gradient-to-r from-black to-black fixed top-0 h-screen w-full flex flex-col box-border py-5 px-10 border-b-[1px] border-amber-50"];
