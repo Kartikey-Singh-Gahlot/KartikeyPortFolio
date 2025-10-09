cb8723b7ab9c20d6fda170f72c626cb8cc01584b
Kartikey Singh Gahlot <decten.kartz@gmail.com>
2025-10-09 10:23:01 +0530

    added smooth scrolling

--- frontend/src/List.jsx
+++ frontend/src/List.jsx
@@ -6,12 +6,12 @@ export default function List({mainLi, subLi, link, linkTarget, subLink, onClk}){
     let subItems = subLi.map((i,idx)=>{return <a href={subLink[idx]}><li className="hover:bg-blue-700  hover:border-black text-[15px] text-amber-50 w-full py-1 px-6 text-center border-[1px] rounded-3xl border-amber-50 bg-black my-2">{i}</li> </a>});
     return (
     <ul>
-        <div className="relative mainBox">
-         <li className="hover:bg-blue-700 min-[780px]:w-full text-[15px] border-[1px] border-black text-amber-50 px-6 py-2 w-fit rounded-3xl" onClick={onClk}> <a href={link} target={linkTarget}>{mainLi}</a> </li>
+        <li className="relative mainBox">
+         <h1 className="hover:bg-blue-700 min-[780px]:w-full text-[15px] border-[1px] border-black text-amber-50 px-6 py-2 w-fit rounded-3xl" onClick={onClk}> <a href={link} target={linkTarget}>{mainLi}</a> </h1>
          <ul className="absolute subBox w-full left-1">
             {subItems}
          </ul>
-        </div>
+        </li>
     </ul>
       
     );
